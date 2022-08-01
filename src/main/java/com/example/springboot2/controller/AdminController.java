a583d2302f0331a2450aa83d16c93747c65180b3
Park Yu Ri <spdlqjdk1012@naver.com>
2022-08-01 11:48:44 +0900

    모든 컨트롤러 model and view 로 변경

--- src/main/java/com/example/springboot2/controller/AdminController.java
+++ src/main/java/com/example/springboot2/controller/AdminController.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Map;
@@ -18,11 +20,11 @@ public class AdminController {
     @Autowired
     MemberService memberService;
 
-    @RequestMapping(value = "/memberList")
-    public String memberList(Model model
-    ){
+    @GetMapping(value = "/memberList")
+    public ModelAndView memberList(){
+        ModelAndView mv = new ModelAndView("admin/memberList");
         List<Map<String, Object>> memberList = memberService.selectMember();
-        model.addAttribute("memberList", memberList);
-        return "admin/memberList";
+        mv.addObject("memberList", memberList);
+        return mv;
     }
 }
--- src/main/java/com/example/springboot2/controller/MainController.java
+++ src/main/java/com/example/springboot2/controller/MainController.java
@@ -10,8 +10,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -27,23 +29,27 @@ public class MainController {
     @Autowired
     MemberService memberService;
     @GetMapping(value = "/main")
-    public String main(){
-        return "user/main";
+    public ModelAndView main(){
+        ModelAndView memberListView = new ModelAndView("user/main");
+        return memberListView;
     }
 
     @GetMapping(value = "/login/form")
-    public String loginForm(){
-        return "user/login";
+    public ModelAndView loginForm(){
+        ModelAndView memberListView = new ModelAndView("user/login");
+        return memberListView;
     }
 
-    @RequestMapping(value="/login/loginProcess")
-    public String loginProcess(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("test login");
+    @PostMapping(value="/login/loginProcess")
+    public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView mv = new ModelAndView();
+
         String username = ""+request.getParameter("username");
         String password = ""+request.getParameter("password");
         Map<String, Object> mem = memberService.selectMemberByIdAndPw(username, password);
         if(mem==null){
-            return "redirect:/login/form";
+            mv.setViewName("redirect:/login/form");
+            return mv;
         }
         String midx = ""+mem.get("midx");
         TokenResponse jwt = jwtTokenProvider.createToken(username, midx);
@@ -53,10 +59,11 @@ public class MainController {
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         response.addCookie(cookie);
-        return "redirect:/main";
+        mv.setViewName("redirect:/main");
+        return mv;
     }
-    @RequestMapping(value="/logoutProcess")
-    public String loginlogout(HttpServletRequest request, HttpServletResponse response) {
+    @GetMapping(value="/logoutProcess")
+    public ModelAndView loginlogout(HttpServletRequest request, HttpServletResponse response) {
         Cookie[] cookies = request.getCookies();
         if(cookies!=null){
             for (Cookie c : cookies) {
@@ -71,20 +78,22 @@ public class MainController {
                 }
             }
         }
-        return "redirect:/login/form";
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:/login/form");
+        return mv;
     }
 
-    @RequestMapping(value="/myInfo")
-    public String myInfo(@AuthenticationPrincipal CustomUserDetails userDetail, Model model) {
-        model.addAttribute("nickname", userDetail.getUsername());
-        model.addAttribute("password", userDetail.getPassword());
-        return "user/myInfo";
+    @GetMapping(value="/myInfo")
+    public ModelAndView myInfo(@AuthenticationPrincipal CustomUserDetails userDetail) {
+        ModelAndView memberListView = new ModelAndView("user/myInfo");
+        memberListView.addObject("nickname", userDetail.getUsername());
+        memberListView.addObject("password", userDetail.getPassword());
+        return memberListView;
     }
 
-    @RequestMapping(value="/login/deny")
-    public String deny(@AuthenticationPrincipal CustomUserDetails userDetail, Model model) {
-        model.addAttribute("nickname", userDetail.getUsername());
-        model.addAttribute("password", userDetail.getPassword());
-        return "user/deny";
+    @GetMapping(value="/login/deny")
+    public ModelAndView deny(@AuthenticationPrincipal CustomUserDetails userDetail, Model model) {
+        ModelAndView memberListView = new ModelAndView("user/deny");
+        return memberListView;
     }
 }
